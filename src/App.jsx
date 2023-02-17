d9c0e48dfb6cef915cdc07e4156c29119c921615
Alisher-Umarov <umarovalisher132@gmail.com>
2023-02-17 23:38:49 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  const [open, setOpen] = useState(0);
+
   return (
-    <div>App</div>
+    <div>
+      <h2>count {open}</h2>
+
+      <button onClick={() => setOpen(open + 1)}>+</button>
+      <button onClick={() => setOpen(open - 1)}>-</button>
+    </div>
   )
 }
 
